7806b4ef339084510268b1fadaf9428ae827c091
aldairyasser <aldair.meza@finwave.es>
2025-07-22 16:36:20 -0500

    main

--- memorix/lib/main.dart
+++ memorix/lib/main.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
+import 'core/di_container.dart';
+import 'core/hive_init.dart';
+import 'presentation/pages/deck_selector_page.dart';
+import 'presentation/pages/study_page.dart';
+import 'domain/entities/deck.dart';
+import 'domain/usecases/get_all_decks.dart';
+import 'presentation/viewmodels/study_view_model.dart';
 
-void main() {
-  runApp(const MainApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initHive();
+  await initDI();
+
+  runApp(const SmartCardsApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SmartCardsApp extends StatelessWidget {
+  const SmartCardsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'SmartCards',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const HomeRouter(),
+    );
+  }
+}
+
+class HomeRouter extends StatelessWidget {
+  const HomeRouter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DeckSelectorPage(
+      getDecks: sl<GetAllDecksUseCase>(),
+      onDeckSelected: (Deck selectedDeck) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => StudyPage(
+              deckId: selectedDeck.id,
+              viewModel: sl<StudyViewModel>(),
+              getAllDecks: sl<GetAllDecksUseCase>(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
